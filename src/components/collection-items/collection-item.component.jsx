ad9f8d2dd1a4051dea92bad5d71eb60a17b65660
Jigar Mehta <jigarmehta3998@gmail.com>
2020-11-19 22:55:52 +0530

    updated shop data with new data, updated card utils to group items in cart reducer

--- src/components/collection-items/collection-item.component.jsx
+++ src/components/collection-items/collection-item.component.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import CustomButton from '../custom-button/custom-button.component'
+import {connect} from 'react-redux'
+import {addItem} from '../../redux/cart/cart.actions'
 import './collection-item.styles.scss'
 
-const CollectionItem = ({id, name, price, imageUrl})=>(
+const CollectionItem = ({item, addItem})=>{
+    
+    const {imageUrl,name,price} =  item
+    return (  
     <div className='collection-item'>
         <div className='image' style={{
             backgroundImage : `url(${imageUrl})`
@@ -14,8 +20,12 @@ const CollectionItem = ({id, name, price, imageUrl})=>(
                 {price}
             </span>  
         </div>        
-
+        <CustomButton onClick={()=>addItem(item)} inverted>Add to Cart</CustomButton>
     </div>
-)
+)}
 
-export default CollectionItem
+const mapDispatchToProps = dispatch =>({
+    addItem: item=> dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem)
--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -66,37 +66,37 @@ const FOOD_DATA = [
       routeName: 'chinese',
       items: [
         {
-          id: 1,
+          id: 10,
           name: 'Chinese Bhel',
           imageUrl: 'https://i0.wp.com/myvegetarianroots.com/wp-content/uploads/2019/10/DSC_0016.jpg?w=2400&ssl=1',
           price: 22
         },
         {
-          id: 2,
+          id: 11,
           name: 'Manchurian Dry',
           imageUrl: 'https://i2.wp.com/vegecravings.com/wp-content/uploads/2017/03/veg-manchurian-dry-recipe-step-by-step-instructions-10.jpg?w=1612&quality=65&strip=all&ssl=1',
           price: 28
         },
         {
-          id: 3,
+          id: 12,
           name: 'Hakka Noodles',
           imageUrl: 'https://www.sanjeevkapoor.com/UploadFiles/RecipeImages/Vegetable-Hakka-Noodles-Best-of-Chinese-Cooking.JPG',
           price: 11
         },
         {
-          id: 4,
+          id: 13,
           name: 'Fried Rice',
           imageUrl: 'https://www.cookwithmanali.com/wp-content/uploads/2017/04/Indo-Chinese-Fried-Rice-500x500.jpg',
           price: 16
         },
         {
-          id: 5,
+          id: 14,
           name: 'Manchurian Fried Rice',
           imageUrl: 'https://ratlamee.com/wp-content/uploads/2018/10/Manchurian-Fried-Rice.jpg',
           price: 16
         },
         {
-          id: 6,
+          id: 15,
           name: 'Schezwan Rice',
           imageUrl: 'https://i0.wp.com/vegecravings.com/wp-content/uploads/2018/03/Veg-Schezwan-Fried-Rice-Recipe-Step-By-Step-Instructions.jpg?w=1612&quality=65&strip=all&ssl=1',
           price: 16
@@ -109,31 +109,31 @@ const FOOD_DATA = [
       routeName: 'south-indian',
       items: [
         {
-          id: 1,
+          id: 16,
           name: 'Paper Dosa',
           imageUrl: 'http://www.wallpaperspick.com/wp-content/uploads/2015/07/Dosa-HD-Wallpaper-01387-540x338.jpg',
           price: 12
         },
         {
-          id: 2,
+          id: 17,
           name: 'Idli',
           imageUrl: 'https://cdn.pixabay.com/photo/2017/06/16/11/38/breakfast-2408818_960_720.jpg',
           price: 9
         },
         {
-          id: 3,
+          id: 18,
           name: 'Menduvada',
           imageUrl: 'https://im.rediff.com/travel-living/2015/apr/144302c33c547d28928409d18e3cf130093b56e8.jpg?w=670&h=900',
           price: 9
         },
         {
-          id: 4,
+          id: 19,
           name: 'Uttapam',
           imageUrl: 'https://pull-revisfoodography.netdna-ssl.com/wp-content/uploads/2015/05/onion-uttapam-1.jpg',
           price: 16
         },
         {
-          id: 5,
+          id: 20,
           name: 'Masala Dosa',
           imageUrl: 'https://ratlamee.com/wp-content/uploads/2018/10/Mysore-Masala-Dosa-3.jpg',
           price: 18
@@ -146,43 +146,43 @@ const FOOD_DATA = [
       routeName: 'gujarati',
       items: [
         {
-          id: 1,
+          id: 21,
           name: 'Potato Sabzi',
           imageUrl: 'https://www.werecipes.com/app/uploads/2014/06/Rasawala-Bateta-Nu-Shaak-Recipe.jpg',
           price: 25
         },
         {
-          id: 2,
+          id: 22,
           name: 'Sev-Tameta Sabzi',
           imageUrl: 'https://kalpskitchen.com/wp-content/uploads/2017/02/sev-tomato-recipe-768x512.jpg',
           price: 25
         },
         {
-          id: 3,
+          id: 23,
           name: 'Roti',
           imageUrl: 'https://ratlamee.com/wp-content/uploads/2018/10/Tawa-Plain-Roti-2.jpg',
           price: 5
         },
         {
-          id: 4,
+          id: 24,
           name: 'Paratha',
           imageUrl: 'https://archanaskitchen.com/images/archanaskitchen/0-Archanas-Kitchen-Recipes/2017/Extras/Tawa_Paratha_Recipe-7956.jpg',
           price: 7
         },
         {
-          id: 5,
+          id: 25,
           name: 'Thepla',
           imageUrl: 'https://ashikitchen.com/wp-content/uploads/2017/09/methi-thepla-696x522.jpg',
           price: 4
         },
         {
-          id: 6,
+          id: 26,
           name: 'Bhakhri',
           imageUrl: 'https://img2.exportersindia.com/product_images/bc-full/2020/1/6358294/dry-bhakhri-1579340239-5260145.jpeg',
           price: 5
         },
         {
-          id: 7,
+          id: 27,
           name: 'Masala Papad',
           imageUrl: 'https://www.merisaheli.com/wp-content/uploads/2018/01/masala_papad-1-685x467.jpg',
           price: 2
@@ -195,37 +195,37 @@ const FOOD_DATA = [
       routeName: 'fast-food',
       items: [
         {
-          id: 1,
+          id: 28,
           name: 'Pizza',
           imageUrl: 'https://hdwallpaperim.com/wp-content/uploads/2017/08/31/151404-pizza-748x421.jpg',
           price: 25
         },
         {
-          id: 2,
+          id: 29,
           name: 'Burger',
           imageUrl: 'https://c4.wallpaperflare.com/wallpaper/894/492/7/mcdonalds-burger-hd-wallpaper-preview.jpg',
           price: 20
         },
         {
-          id: 3,
+          id: 30,
           name: 'Sandwich',
           imageUrl: 'https://www.itl.cat/pngfile/big/164-1645612_big-sandwich-pictures-photos-hd-image-of-sandwich.jpg',
           price: 25
         },
         {
-          id: 4,
+          id: 31,
           name: 'Hotdog',
           imageUrl: 'https://recipetreasure.com/wp-content/uploads/2013/05/veggie-hot-dog-ready-3.jpg',
           price: 25
         },
         {
-          id: 5,
+          id: 32,
           name: 'French-fries',
           imageUrl: 'https://img.apmcdn.org/4b2716626c9ff3f6e5dfebe520eb592c33cf1e7b/uncropped/b5464d-splendid-table-french-fries.jpg',
           price: 20
         },
         {
-          id: 6,
+          id: 33,
           name: 'Garlic Bread',
           imageUrl: 'https://www.maggi.in/sites/default/files/2020-04/Cheesy-Garlic-Bread-1082x461.jpg',
           price: 25
